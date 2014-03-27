3a6cdb57fb2b83acac396756fe04140ca1350f90
valeriya <valeriya@htc-cs.com>
2014-03-27 21:48:01 +0400

    Изменена подпись о голосовании, css

--- install/components/htc/sociallikesbx/templates/.default/lang/ru/template.php
+++ install/components/htc/sociallikesbx/templates/.default/lang/ru/template.php
@@ -1,7 +1,7 @@
 <?
 $MESS['SOCSERV_AS_USER_NOTE'] = 'Вы можете проголосовать, если зарегистрированы на одном из этих сервисов:';
 $MESS['AUTH_LOGOUT_BUTTON'] = 'Выйти';
-$MESS['USER_CAN_NOT_VOTE'] = 'Сегодня вы уже проголосовали';
+$MESS['USER_CAN_NOT_VOTE'] = 'Вы уже проголосовали';
 $MESS['USER_CAN_VOTE'] = 'Голосовать за работу';
 $MESS['METADATA'] = '<meta property="og:title" content="%s" />
                     <meta property="og:url" content="%s" />
